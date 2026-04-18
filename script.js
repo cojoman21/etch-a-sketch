d566311cd8126fed4d82da8eb80d7418a3f7c9fd
cojoman21 <cojocaru.petrubd@gmail.com>
2026-04-18 11:13:46 +0300

    Add color mode switch button Add a toggle button to switch between Single and Random color.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let mainDiv = document.createElement("div");
+const mainDiv = document.createElement("div");
 
 let gridSize = 16;
 
@@ -20,9 +20,9 @@ function createGrid(gridSize) {
     }
 }
 
-let squares = document.querySelectorAll(".square");
+// let squares = document.querySelectorAll(".square");
 
-function randomRGB() {
+const randomRGB = () => {
     let min = 0;
     let max = 255;
 
@@ -30,13 +30,22 @@ function randomRGB() {
     let gValue = Math.floor(Math.random() * (max - min + 1)) + min;
     let bValue = Math.floor(Math.random() * (max - min + 1)) + min;
     return `rgb(${rValue}, ${gValue}, ${bValue})`;
-}
+};
+
+const singleColor = () => {
+    return `rgb(${100}, ${100}, ${100})`;
+};
+
+// Initial setup 16x16, random colors
+createGrid(gridSize);
+let colorMode = randomRGB;
+setTrailEffect();
 
 function setTrailEffect() {
     const squares = document.querySelectorAll(".square");
     squares.forEach((square) => {
         square.addEventListener("mouseenter", function (event) {
-            event.target.style.backgroundColor = randomRGB();
+            event.target.style.backgroundColor = colorMode();
             setTimeout(() => {
                 event.target.style.backgroundColor = "";
             }, 1000);
@@ -44,16 +53,29 @@ function setTrailEffect() {
     });
 }
 
-createGrid(gridSize);
-setTrailEffect();
+const gridSizeBtn = document.querySelector("#gridSize");
 
-const btn = document.querySelector("button");
-
-btn.addEventListener("click", () => {
+gridSizeBtn.addEventListener("click", () => {
     gridSize = prompt("Enter grid size(max 100): ", 16);
+    if (gridSize > 100) gridSize = 100;
     while (mainDiv.firstChild) {
         mainDiv.removeChild(mainDiv.firstChild);
     }
     createGrid(gridSize);
     setTrailEffect();
+});
+
+const colorModeBtn = document.querySelector("#colorMode");
+
+colorModeBtn.addEventListener("click", (event) => {
+    if (event.target.innerText.toLowerCase() === "single") {
+        event.target.innerText = "Random";
+        colorMode = randomRGB;
+        return;
+    }
+    if (event.target.innerText.toLowerCase() === "random") {
+        event.target.innerText = "Single";
+        colorMode = singleColor;
+        return;
+    }
 });
