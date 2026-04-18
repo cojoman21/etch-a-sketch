d2e79e527fa6dfcceb8ff165597b9959066e4ed0
cojoman21 <cojocaru.petrubd@gmail.com>
2026-04-18 09:27:28 +0300

    Add trail effect with setTimeout()

--- script.js
+++ script.js
@@ -17,9 +17,23 @@ for (let i = 0; i < 16; i++) {
 
 const squares = document.querySelectorAll(".square");
 
+function randomRGB() {
+    let min = 0;
+    let max = 255;
+
+    let rValue = Math.floor(Math.random() * (max - min + 1)) + min;
+    let gValue = Math.floor(Math.random() * (max - min + 1)) + min;
+    let bValue = Math.floor(Math.random() * (max - min + 1)) + min;
+    return `rgb(${rValue}, ${gValue}, ${bValue})`;
+}
+
+// Trail
 squares.forEach((square) => {
     square.addEventListener("mouseenter", function (event) {
-        event.target.style.backgroundColor = "#fff";
+        event.target.style.backgroundColor = randomRGB();
         console.log("touched");
+        setTimeout(() => {
+            event.target.style.backgroundColor = "";
+        }, 1000);
     });
 });
